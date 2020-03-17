d81dcec235dd57899247286283f52f0b2653ec8c
Daphne Kramer <99046027@mydavinci.nl>
2020-03-17 19:16:12 +0100

    Blok 3
    Week 4 tm 5

--- sql.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `characters` (`id`, `name`, `avatar`, `health`, `bio`, `color`, `attack`, `defense`, `weapon`, `armor`) VALUES
-("11", 'Daphne', 'daphne.jpg', 123, 'Daphne is 23 jaar oud en doet de opleiding Software developer', 'lightblue', 33, 67, 'Noor', 'kleding');
-
